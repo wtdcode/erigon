531b664fcdba144139b196a030061400dfcccef3
alex.sharov <AskAlexSharov@gmail.com>
2023-12-01 14:21:07 +0700

    save

--- eth/stagedsync/exec3.go
+++ eth/stagedsync/exec3.go
@@ -714,6 +714,8 @@ Loop:
 				// use history reader instead of state reader to catch up to the tx where we left off
 				HistoryExecution: offsetFromBlockBeginning > 0 && txIndex < int(offsetFromBlockBeginning),
 			}
+			fmt.Printf("alex: %t, %d\n", txTask.HistoryExecution, offsetFromBlockBeginning)
+
 			//if txTask.HistoryExecution { // nolint
 			//	fmt.Printf("[dbg] txNum: %d, hist=%t\n", txTask.TxNum, txTask.HistoryExecution)
 			//}
