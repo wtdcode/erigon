499618f889bae0d4fd0d901401e253683c5d52b2
alex.sharov <AskAlexSharov@gmail.com>
2023-12-11 11:32:57 +0700

    save

--- core/state/rw_v3.go
+++ core/state/rw_v3.go
@@ -463,8 +463,8 @@ type StateWriterV3 struct {
 
 func NewStateWriterV3(rs *StateV3) *StateWriterV3 {
 	return &StateWriterV3{
-		rs: rs,
-		//trace: true,
+		rs:    rs,
+		trace: true,
 	}
 }
 
@@ -559,7 +559,7 @@ type StateReaderV3 struct {
 
 func NewStateReaderV3(rs *StateV3) *StateReaderV3 {
 	return &StateReaderV3{
-		//trace:     true,
+		trace:     true,
 		rs:        rs,
 		readLists: newReadList(),
 		composite: make([]byte, 20+32),
