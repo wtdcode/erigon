3270815e15342670db53e578afa643dff07050f2
alex.sharov <AskAlexSharov@gmail.com>
2023-12-25 15:28:34 +0700

    save

--- turbo/app/snapshots_cmd.go
+++ turbo/app/snapshots_cmd.go
@@ -314,7 +314,7 @@ func doDebugKey(cliCtx *cli.Context) error {
 				}
 
 				i++
-				if i%1_000_000 == 0 {
+				if i%100_000 == 0 {
 					log.Warn(fmt.Sprintf("[dbg] step=%d, key=%x", minStep, key))
 				}
 			}
