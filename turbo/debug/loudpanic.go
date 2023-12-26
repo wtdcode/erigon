63bbabbe6392eb2f9a05e522a860c9bc6e5e9166
alex.sharov <AskAlexSharov@gmail.com>
2023-12-26 09:56:53 +0700

    save

--- turbo/debug/loudpanic.go
+++ turbo/debug/loudpanic.go
@@ -22,6 +22,6 @@ import (
 
 // LoudPanic panics in a way that gets all goroutine stacks printed on stderr.
 func LoudPanic(x interface{}) {
-	debug.SetTraceback("1")
+	debug.SetTraceback("0")
 	panic(x)
 }
