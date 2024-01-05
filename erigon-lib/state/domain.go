a0f6663994dd760ccb55522f42358982283e09db
alex.sharov <AskAlexSharov@gmail.com>
2024-01-05 14:30:27 +0700

    save

--- erigon-lib/state/domain.go
+++ erigon-lib/state/domain.go
@@ -1677,9 +1677,10 @@ func (dc *DomainContext) getLatestFromFiles(filekey []byte) (v []byte, found boo
 			return nil, false, err
 		}
 		if !found {
-			if traceGetLatest == dc.d.filenameBase && i <= 1 {
+			//if traceGetLatest == dc.d.filenameBase && i == 0 {
+			if i == 0 {
 				B++
-				//fmt.Printf("GetLatest(%s, %x) -> not found in file %s (false positive existence idx)\n", dc.d.filenameBase, filekey, dc.files[i].src.decompressor.FileName())
+				fmt.Printf("GetLatest(%s, %x) -> not found in file %s (false positive existence idx)\n", dc.d.filenameBase, filekey, dc.files[i].src.decompressor.FileName())
 				//fmt.Printf("bloom false-positive probability: %s, %f, a-b=%d-%d\n", dc.files[i].src.existence.FileName, dc.files[i].src.existence.filter.FalsePosititveProbability(), A, B)
 
 				//m := bloomfilter.OptimalM(dc.files[i].src.existence.filter.N()*10, 0.01)
