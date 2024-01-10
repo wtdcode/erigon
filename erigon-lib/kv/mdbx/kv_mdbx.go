7ac519ad3e53024df7b2347eaecc494342429186
alex.sharov <AskAlexSharov@gmail.com>
2024-01-10 16:12:15 +0700

    save

--- erigon-lib/kv/mdbx/kv_mdbx.go
+++ erigon-lib/kv/mdbx/kv_mdbx.go
@@ -293,20 +293,6 @@ func (opts MdbxOpts) Open(ctx context.Context) (kv.RwDB, error) {
 		return nil, fmt.Errorf("%w, label: %s, trace: %s", err, opts.label.String(), stack2.Trace().String())
 	}
 
-	// mdbx will not change pageSize if db already exists. means need read real value after env.open()
-	in, err := env.Info(nil)
-	if err != nil {
-		return nil, fmt.Errorf("%w, label: %s, trace: %s", err, opts.label.String(), stack2.Trace().String())
-	}
-
-	opts.pageSize = uint64(in.PageSize)
-	opts.mapSize = datasize.ByteSize(in.MapSize)
-	if opts.label == kv.ChainDB {
-		opts.log.Info("[db] open", "lable", opts.label, "sizeLimit", opts.mapSize, "pageSize", opts.pageSize)
-	} else {
-		opts.log.Debug("[db] open", "lable", opts.label, "sizeLimit", opts.mapSize, "pageSize", opts.pageSize)
-	}
-
 	// erigon using big transactions
 	// increase "page measured" options. need do it after env.Open() because default are depend on pageSize known only after env.Open()
 	if !opts.HasFlag(mdbx.Readonly) {
@@ -336,6 +322,25 @@ func (opts MdbxOpts) Open(ctx context.Context) (kv.RwDB, error) {
 				return nil, err
 			}
 		}
+	}
+
+	// mdbx will not change pageSize if db already exists. means need read real value after env.open()
+	in, err := env.Info(nil)
+	if err != nil {
+		return nil, fmt.Errorf("%w, label: %s, trace: %s", err, opts.label.String(), stack2.Trace().String())
+	}
+
+	opts.pageSize = uint64(in.PageSize)
+	opts.mapSize = datasize.ByteSize(in.MapSize)
+	if opts.label == kv.ChainDB {
+		opts.log.Info("[db] open", "lable", opts.label, "sizeLimit", opts.mapSize, "pageSize", opts.pageSize)
+	} else {
+		opts.log.Debug("[db] open", "lable", opts.label, "sizeLimit", opts.mapSize, "pageSize", opts.pageSize)
+	}
+
+	// erigon using big transactions
+	// increase "page measured" options. need do it after env.Open() because default are depend on pageSize known only after env.Open()
+	if !opts.HasFlag(mdbx.Readonly) {
 
 		var dirtySpace uint64
 		if opts.dirtySpace > 0 {
