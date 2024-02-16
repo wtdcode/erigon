806fbeb89be5f719a17b754396a999b80c10be54
alex.sharov <AskAlexSharov@gmail.com>
2024-02-16 12:56:05 +0700

    save

--- polygon/bor/bor.go
+++ polygon/bor/bor.go
@@ -1323,6 +1323,7 @@ func (c *Bor) checkAndCommitSpan(
 
 		return c.fetchAndCommitSpan(currentSpan.ID, state, header, chain, syscall)
 	}
+	log.Warn("[dbg] isSprintStart3", "currentSpan.EndBlock", currentSpan.EndBlock)
 
 	// if current block is first block of last sprint in current span
 	sprintLength := c.config.CalculateSprintLength(headerNumber)
@@ -1330,6 +1331,7 @@ func (c *Bor) checkAndCommitSpan(
 		log.Warn("[dbg] checkAndCommitSpan2", "block", headerNumber)
 		return c.fetchAndCommitSpan(currentSpan.ID+1, state, header, chain, syscall)
 	}
+	log.Warn("[dbg] isSprintStart4")
 
 	return nil
 }
