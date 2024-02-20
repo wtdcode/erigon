c15ec6b85bf6d940fa20d1cab4e1eee60c47aa63
alex.sharov <AskAlexSharov@gmail.com>
2024-02-20 13:24:18 +0700

    save

--- turbo/app/snapshots_cmd.go
+++ turbo/app/snapshots_cmd.go
@@ -362,12 +362,6 @@ func doIntegrity(cliCtx *cli.Context) error {
 
 	blockReader, _ := blockRetire.IO()
 
-	search := common.HexToHash("0x23b6c3c6c254a0952a59c85f00cac9c8b080f0d65c7df0f0070c0b47eda6c3dc")
-	chainDB.View(ctx, func(tx kv.Tx) error {
-		bn, ok, err := blockReader.EventLookup(ctx, tx, search)
-		return nil
-	})
-
 	if err := integrity.SnapBorEventsRead(chainDB, blockReader, ctx, false); err != nil {
 		return err
 	}
