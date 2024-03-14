7c8eef8f57423ad6f00213f480d5ce94daff87e8
alex.sharov <AskAlexSharov@gmail.com>
2024-03-14 10:06:47 +0700

    save

--- erigon-lib/downloader/downloader.go
+++ erigon-lib/downloader/downloader.go
@@ -952,8 +952,6 @@ func (d *Downloader) mainLoop(silent bool) error {
 			}
 			d.lock.RUnlock()
 
-			fmt.Printf("[dbg] waiting %s\n", waiting)
-			fmt.Printf("[dbg] downloading %s\n", d.downloading)
 			for _, t := range available {
 
 				torrentInfo, _ := d.torrentInfo(t.Name())
