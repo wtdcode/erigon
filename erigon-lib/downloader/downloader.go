f831e96703f1032117e97082c04243c88bace173
alex.sharov <AskAlexSharov@gmail.com>
2024-03-14 11:21:26 +0700

    save

--- erigon-lib/downloader/downloader.go
+++ erigon-lib/downloader/downloader.go
@@ -1101,7 +1101,6 @@ func (d *Downloader) ReCalcStats(interval time.Duration) {
 		case <-t.GotInfo():
 		default: // if some torrents have no metadata, we are for-sure uncomplete
 			stats.Completed = false
-			noMetadata = append(noMetadata, t.Name())
 			continue
 		}
 
@@ -1144,38 +1143,6 @@ func (d *Downloader) ReCalcStats(interval time.Duration) {
 		rates, peers := getPeersRatesForlogs(peersOfThisFile, torrentName)
 
 		if !torrentComplete {
-			if info, err := d.torrentInfo(torrentName); err == nil {
-				updateStats := t.Info() == nil
-
-				if updateStats {
-					dbInfo++
-				}
-
-				if info.Completed != nil && info.Completed.Before(time.Now()) {
-					if info.Length != nil {
-						if updateStats {
-							stats.MetadataReady++
-							stats.BytesTotal += uint64(*info.Length)
-						}
-
-						if fi, err := os.Stat(filepath.Join(d.SnapDir(), t.Name())); err == nil {
-							if torrentComplete = (fi.Size() == *info.Length); torrentComplete {
-								infoRead := t.Stats().BytesReadData
-								if updateStats || infoRead.Int64() == 0 {
-									stats.BytesCompleted += uint64(*info.Length)
-								}
-								dbComplete++
-								progress = float32(100)
-							}
-						}
-					}
-				}
-			} else if _, ok := d.webDownloadInfo[torrentName]; ok {
-				stats.MetadataReady++
-			} else {
-				noMetadata = append(noMetadata, torrentName)
-			}
-
 			if progress == 0 {
 				zeroProgress = append(zeroProgress, torrentName)
 			}
