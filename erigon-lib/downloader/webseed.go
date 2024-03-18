7ccebe3be2ca11a1593c9bcc39f2f80634ec4ec3
alex.sharov <AskAlexSharov@gmail.com>
2024-03-18 16:47:36 +0700

    save

--- erigon-lib/downloader/webseed.go
+++ erigon-lib/downloader/webseed.go
@@ -14,6 +14,7 @@ import (
 
 	"github.com/c2h5oh/datasize"
 	"github.com/ledgerwatch/erigon-lib/chain/snapcfg"
+	"github.com/ledgerwatch/erigon-lib/common/dbg"
 	"golang.org/x/sync/errgroup"
 
 	"github.com/anacrolix/torrent/bencode"
@@ -119,6 +120,7 @@ func (d *WebSeeds) makeWebSeedUrls(listsOfFiles []snaptype.WebSeedsFromProvider,
 
 		}
 	}
+	log.Warn("[dbg] makeTorrentUrls", "l", len(webSeedUrls), "stack", dbg.Stack())
 
 	d.lock.Lock()
 	defer d.lock.Unlock()
