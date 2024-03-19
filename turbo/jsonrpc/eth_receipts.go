28b7da4de3d655d2d11a66636a41f274c054dbc8
alex.sharov <AskAlexSharov@gmail.com>
2024-03-19 13:36:12 +0700

    save

--- turbo/jsonrpc/eth_receipts.go
+++ turbo/jsonrpc/eth_receipts.go
@@ -201,10 +201,7 @@ func (api *APIImpl) GetLogs(ctx context.Context, crit filters.FilterCriteria) (t
 			//blockLogs = append(blockLogs, filtered...)
 		}
 		if casted, ok := it.(kv.Closer); ok {
-			log.Warn("[dbg] can close", "bn", blockNumber)
 			casted.Close()
-		} else {
-			log.Warn("[dbg] can't close", "bn", blockNumber)
 		}
 		if len(blockLogs) == 0 {
 			continue
