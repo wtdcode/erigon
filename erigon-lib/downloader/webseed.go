14ba5a1cc5a7b9467053208c06dfe5ad98712444
alex.sharov <AskAlexSharov@gmail.com>
2024-03-19 10:26:15 +0700

    save

--- erigon-lib/downloader/webseed.go
+++ erigon-lib/downloader/webseed.go
@@ -42,9 +42,6 @@ type WebSeeds struct {
 }
 
 func (d *WebSeeds) Discover(ctx context.Context, urls []*url.URL, files []string, rootDir string) {
-	if d.torrentFiles.newDownloadsAreProhibited() {
-		return
-	}
 	listsOfFiles := d.constructListsOfFiles(ctx, urls, files)
 	torrentMap := d.makeTorrentUrls(listsOfFiles)
 	webSeedMap := d.downloadTorrentFilesFromProviders(ctx, rootDir, torrentMap)
