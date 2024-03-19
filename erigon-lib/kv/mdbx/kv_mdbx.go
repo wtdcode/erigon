cd6888280db6ac0a0c799f75a06cb26b28942109
alex.sharov <AskAlexSharov@gmail.com>
2024-03-19 14:05:34 +0700

    save

--- erigon-lib/kv/mdbx/kv_mdbx.go
+++ erigon-lib/kv/mdbx/kv_mdbx.go
@@ -1871,7 +1871,7 @@ func (s *cursor2iter) init(table string, tx kv.Tx) (*cursor2iter, error) {
 func (s *cursor2iter) Close() {
 	if s.c != nil {
 		s.c.Close()
-		delete(s.tx.streams, s.id)
+		//delete(s.tx.streams, s.id)
 		s.c = nil
 	}
 }
@@ -1978,7 +1978,7 @@ func (s *cursorDup2iter) init(table string, tx kv.Tx) (*cursorDup2iter, error) {
 func (s *cursorDup2iter) Close() {
 	if s.c != nil {
 		s.c.Close()
-		delete(s.tx.streams, s.id)
+		//delete(s.tx.streams, s.id)
 		s.c = nil
 	}
 }
